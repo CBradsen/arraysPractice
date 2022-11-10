6d9497b710fa94fedeafb09d2f9eefae4ba1cde7
Carol Bradsen <carolbradsen@gmail.com>
2022-11-09 21:26:24 -0700

    Add three arrays

---
+++ arrays.js
@@ -0,0 +1,4 @@
+/ declare a variable that stores an Array of 4 strings
+var cats = ["Calico", "Alley", "Barn", "Unicorn"]
+var houseSeatsOut = [23, 50, 102, 87]
+var NotSure = [How do you put Booleans in an array?]
