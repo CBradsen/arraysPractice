bbd3e1bf82308f9232d4a67b5733d105186d6432
Carol Bradsen <carolbradsen@gmail.com>
2022-11-09 21:38:41 -0700

    Try the push method.

--- arrays.js
+++ arrays.js
@@ -1,4 +1,7 @@
-/ declare a variable that stores an Array of 4 strings
+// declare a variable that stores an Array of 4 strings
 var cats = ["Calico", "Alley", "Barn", "Unicorn"]
 var houseSeatsOut = [23, 50, 102, 87]
-var NotSure = [How do you put Booleans in an array?]
+var NotSure = ("I can not find how to store booleans into an array.")
+
+// .push will add these two elements to the end of the array and reutn a new length of the array: 6
+cats.push("Fat", "Furry")
